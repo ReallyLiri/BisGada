4848fd2f6cb529e8f1fa8d30d54f8c8357161f6d
ReallyLiri <reallyliri@gmail.com>
2020-01-07 16:16:54 +0200

    scratch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,122 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useRef} from 'react';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Container = styled.div`
+  position: relative;
+  width: 285px;
+  height: 70px;
+`;
+
+const Canvas = styled.canvas`
+  position: absolute;
+  z-index: 2;
+  width: 100%;
+  height: 100%;
+`;
+
+const Hidden = styled.div`
+  background-color: red;
+`;
+
+const secret = Math.random().toString(16).slice(2, 7).toUpperCase();
+
+export default () => {
+
+    let isDrawing = false;
+    let lastPoint = null;
+    let context = null;
+    const canvasRef = useRef(null);
+
+    const brush = new Image();
+    brush.src = require('./images/brush.png');
+
+    const coverImage = new Image();
+    coverImage.src = require('./images/cover.png');
+
+    const getPosition = (event) => {
+
+        let target = canvasRef.current;
+        let offsetX = 0;
+        let offsetY = 0;
+
+        if (target.offsetParent !== undefined) {
+            while (target = target.offsetParent) {
+                offsetX += target.offsetLeft;
+                offsetY += target.offsetTop;
+            }
+        }
+
+        const x = (event.pageX || (event.touches ? event.touches[0].clientX : offsetX)) - offsetX;
+        const y = (event.pageY || (event.touches ? event.touches[0].clientY : offsetY)) - offsetY;
+        return {x, y};
+    };
+
+    const touchStart = (event) => {
+        isDrawing = true;
+        lastPoint = getPosition(event);
+        context.globalCompositeOperation = 'destination-out';
+    };
+
+    const touchMove = (event) => {
+
+        if (!isDrawing) {
+            return;
+        }
+        event.preventDefault();
+
+        const a = lastPoint;
+        const b = getPosition(event);
+        const dist = Math.sqrt(Math.pow(b.x - a.x, 2) + Math.pow(b.y - a.y, 2));
+        const angle = Math.atan2(b.x - a.x, b.y - a.y);
+        const offsetX = brush.width / 2;
+        const offsetY = brush.height / 2;
+
+        for (let x, y, i = 0; i < dist; i++) {
+            x = a.x + (Math.sin(angle) * i) - offsetX;
+            y = a.y + (Math.cos(angle) * i) - offsetY;
+            context.drawImage(brush, x, y);
+        }
+
+        lastPoint = b;
+    };
+
+    const touchEnd = (event) => {
+        isDrawing = false;
+    };
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        canvas.width = canvas.parentElement.offsetWidth;
+        canvas.height = canvas.parentElement.offsetHeight;
+
+        window.addEventListener('mousedown', touchStart, false);
+        window.addEventListener('touchstart', touchStart, false);
+        window.addEventListener('mousemove', touchMove, false);
+        window.addEventListener('touchmove', touchMove, false);
+        window.addEventListener('mouseup', touchEnd, false);
+        window.addEventListener('touchend', touchEnd, false);
+
+        context = canvas.getContext('2d');
+
+
+        coverImage.onload = () => context.drawImage(coverImage, 0, 0, canvas.width, canvas.height);
+
+        return () => {
+            window.removeEventListener('mousedown', touchStart);
+            window.removeEventListener('touchstart', touchStart);
+            window.removeEventListener('mousemove', touchMove);
+            window.removeEventListener('touchmove', touchMove);
+            window.removeEventListener('mouseup', touchEnd);
+            window.removeEventListener('touchend', touchEnd);
+        }
+    }, []);
+
+    return (
+        <Container>
+            <Canvas ref={canvasRef}/>
+            <Hidden className="secret absolute fill no-select flex justify-center items-center">
+                {secret}
+            </Hidden>
+        </Container>
+    );
 }
-
-export default App;
